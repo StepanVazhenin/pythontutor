618ea9009557b36ed76e91b6cb27a7e6d8541e12
StepanVazhenin <113357032+StepanVazhenin@users.noreply.github.com>
2022-09-12 18:50:13 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from math import sqrt
 
 def distance(x1, y1, x2, y2):
-    return sqrt((x1-x2) ** 2 + (y1-y1) ** 2)
+    return sqrt((x1-x2) ** 2 + (y1-y2) ** 2)
 
 x1 = float(input())
 y1 = float(input())
