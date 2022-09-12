0581b04e2faf3e77b3e9cf85c913a89f709614bf
StepanVazhenin <113357032+StepanVazhenin@users.noreply.github.com>
2022-09-12 19:09:04 +0500

    Capital letters

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-# distance between (x1;y1) (x2;y2)
-from math import sqrt
+# capital letters
 
-def distance(x1, y1, x2, y2):
-    return sqrt((x1-x2) ** 2 + (y1-y2) ** 2)
 
-x1 = float(input())
-y1 = float(input())
-x2 = float(input())
-y2 = float(input())
+def capitalize(i):
+    first_small = i[0].upper()
+    return first_small + i[1:]
 
-print(distance(x1, y1, x2, y2))
+
+words = str(input()).split()
+res = []
+for i in words:
+    res.append(capitalize(i))
+print(' '.join(res))
